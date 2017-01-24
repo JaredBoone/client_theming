c9da1a84a4fb498388ee688a97ff69b26862a10f
jboone <jared.boone@sony.com>
2017-01-23 19:00:06 -0800

    personal build script changes

--- osx/build.sh
+++ osx/build.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 export PATH=/usr/local/Qt-5.4.0/bin/:$PATH
 export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+export WORKING_DIR=~/workspace/temp
+export CLIENT_THEMING_DIR=~/workspace/nextcloud-client_theming
 
 # Cleanup
-cd ~
+cd $WORKING_DIR
 sudo rm -rf build-mac
-sudo rm -rf client
+sudo rm -rf owncloud-client
 sudo rm -rf install
 
 # Clone the desktop client code
-cd ~
-git clone --recursive https://github.com/owncloud/client.git
-cd client
-git checkout v2.2.4
+cd $WORKING_DIR
+git clone --recursive https://github.com/JaredBoone/owncloud-client.git
+cd owncloud-client
+git checkout 2.2.4
 git submodule update --recursive
 
 # Build qtkeychain
-cd ~/client/src/3rdparty/qtkeychain
-cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=/Users/builder/install -DCMAKE_PREFIX_PATH=/Users/builder/Qt/5.4/clang_64 .
+cd $WORKING_DIR/owncloud-client/src/3rdparty/qtkeychain
+cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/install -DCMAKE_PREFIX_PATH=/usr/local/Qt-5.4.0 .
 sudo make install
 
 # Build the client
-cd ~
-cp client_theming/osx/dsa_pub.pem client/admin/osx/sparkle/
+cd $WORKING_DIR
+cp $CLIENT_THEMING_DIR/osx/dsa_pub.pem owncloud-client/admin/osx/sparkle/
 rm -rf build-mac
 mkdir build-mac
 cd build-mac
-cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=/Users/builder/install -DCMAKE_PREFIX_PATH=/Users/builder/Qt/5.4/clang_64 -D SPARKLE_INCLUDE_DIR=/Users/builder/Library/Frameworks/Sparkle.framework/ -D SPARKLE_LIBRARY=/Users/builder/Library/Frameworks/Sparkle.framework/ -D OEM_THEME_DIR=/Users/builder/client_theming/nextcloudtheme -DWITH_CRASHREPORTER=ON -DMIRALL_VERSION_BUILD=1 ../client
+cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/install -DCMAKE_PREFIX_PATH=/usr/local/Qt-5.4.0 -D SPARKLE_INCLUDE_DIR=~/Library/Frameworks/Sparkle.framework/ -D SPARKLE_LIBRARY=~/Library/Frameworks/Sparkle.framework/ -D OEM_THEME_DIR=$CLIENT_THEMING_DIR/shipdrivetheme -DWITH_CRASHREPORTER=ON -DMIRALL_VERSION_BUILD=1 ../owncloud-client -Wno-dev
 make
 sudo make install
-sudo ~/client_theming/client/admin/osx/sign_app.sh ~/install/nextcloud.app 14C6139C495F80E3086E9912410161BFB41F0578
-sudo ./admin/osx/create_mac.sh ../install/ . 3EA9DE660A8EE9ED0852BEEEA29269A22E97D427
+sudo $WORKING_DIR/owncloud-client/admin/osx/sign_app.sh $WORKING_DIR/install/shipdrive.app DA30C098685993A9C483F89D06F2E46B467034A6 PZR8TPXW34
+sudo ./admin/osx/create_mac.sh ../install/ . 053762F12204E448C66A0B3FF6BEBC4E8865DC23
 
 # Generate a sparkle signature for the tbz
-openssl dgst -sha1 -binary < ~/install/*.tbz | openssl dgst -dss1 -sign ~/dsa_priv.pem | openssl enc -base64 > ~/sig.txt
-sudo mv ~/sig.txt ~/install/signature.txt
+openssl dgst -sha1 -binary < $WORKING_DIR/install/*.tbz | openssl dgst -dss1 -sign $CLIENT_THEMING_DIR/osx/dsa_priv.pem | openssl enc -base64 > $WORKING_DIR/sig.txt
+sudo mv $WORKING_DIR/sig.txt $WORKING_DIR/install/signature.txt
