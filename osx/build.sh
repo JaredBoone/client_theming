dc6684e4897a7bfd1b43365814c5b9bf237c3beb
jboone <jared.boone@sony.com>
2017-09-22 15:27:52 -0700

    removed sudo

--- osx/build.sh
+++ osx/build.sh
@@ -6,9 +6,9 @@ export CLIENT_THEMING_DIR=~/workspace/nextcloud-client_theming
 
 # Cleanup
 cd $WORKING_DIR
-sudo rm -rf build-mac
-sudo rm -rf owncloud-client
-sudo rm -rf install
+rm -rf build-mac
+rm -rf owncloud-client
+rm -rf install
 
 # Clone the desktop client code
 cd $WORKING_DIR
@@ -26,7 +26,7 @@ git submodule update --recursive
 # Build qtkeychain
 cd $WORKING_DIR/owncloud-client/src/3rdparty/qtkeychain
 cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/install -DCMAKE_PREFIX_PATH=/usr/local/Qt-5.4.0 .
-sudo make install
+make install
 
 # Build the client
 cd $WORKING_DIR
@@ -36,10 +36,10 @@ mkdir build-mac
 cd build-mac
 cmake -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 -DCMAKE_INSTALL_PREFIX=$WORKING_DIR/install -DCMAKE_PREFIX_PATH=/usr/local/Qt-5.4.0 -D SPARKLE_INCLUDE_DIR=~/Library/Frameworks/Sparkle.framework/ -D SPARKLE_LIBRARY=~/Library/Frameworks/Sparkle.framework/ -D OEM_THEME_DIR=$CLIENT_THEMING_DIR/shipdrivetheme -DWITH_CRASHREPORTER=ON -DMIRALL_VERSION_BUILD=1 ../owncloud-client -Wno-dev
 make
-sudo make install
-sudo $WORKING_DIR/owncloud-client/admin/osx/sign_app.sh $WORKING_DIR/install/shipdrive.app DA30C098685993A9C483F89D06F2E46B467034A6 PZR8TPXW34
-sudo ./admin/osx/create_mac.sh ../install/ . 053762F12204E448C66A0B3FF6BEBC4E8865DC23
+make install
+$WORKING_DIR/owncloud-client/admin/osx/sign_app.sh $WORKING_DIR/install/shipdrive.app DA30C098685993A9C483F89D06F2E46B467034A6 PZR8TPXW34
+./admin/osx/create_mac.sh ../install/ . 053762F12204E448C66A0B3FF6BEBC4E8865DC23
 
 # Generate a sparkle signature for the tbz
 openssl dgst -sha1 -binary < $WORKING_DIR/install/*.tbz | openssl dgst -dss1 -sign $CLIENT_THEMING_DIR/osx/dsa_priv.pem | openssl enc -base64 > $WORKING_DIR/sig.txt
-sudo mv $WORKING_DIR/sig.txt $WORKING_DIR/install/signature.txt
+mv $WORKING_DIR/sig.txt $WORKING_DIR/install/signature.txt
